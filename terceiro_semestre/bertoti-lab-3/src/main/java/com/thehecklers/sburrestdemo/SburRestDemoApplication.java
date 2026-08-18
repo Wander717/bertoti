f8f84d00e4619c5278914d0ac769a006dc4aa56d
Wander717 <wandersonrsantos01@gmail.com>
2026-08-17 22:37:15 -0300

    classe Coffee separada.

--- terceiro_semestre/bertoti-lab-3/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ terceiro_semestre/bertoti-lab-3/src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -80,29 +80,3 @@ class RestApiDemoController {
 		coffees.removeIf(c -> c.getId().equals(id));
 	}
 }
-
-class Coffee {
-	private final String id;
-	private String name;
-
-	public Coffee(String id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public Coffee(String name) {
-		this(UUID.randomUUID().toString(), name);
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-}
